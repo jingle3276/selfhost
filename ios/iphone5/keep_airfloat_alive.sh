c05957d2f71cc99b5587a415b5eb8c85a23f9056
Ying Zhao <jingle3276@hotmail.com>
2024-01-22 00:03:43 -0500

    Update keep_airfloat_alive.sh

--- ios/iphone5/keep_airfloat_alive.sh
+++ ios/iphone5/keep_airfloat_alive.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# To run this script, the user's ssh public key must be added to the ios device's known-host list
+# the ios device must be in static DHCP table with the HOST value
+# IOS device must be jailbroken and have these dependencies: ssh, activator, AirFloat, stouch 
+# the IOS device must the check_wake_airfloat.sh installed on /etc/rc.d/wake_airfloat.sh 
+
+
 #ssh -t root@192.168.3.156 "/etc/rc.d/check_wake_airfloat.sh 30 &"
 #ssh -t root@192.168.3.156 "nohup /etc/rc.d/check_wake_airfloat.sh 30 > /dev/null 2>&1 &"
+
 
 HOST=192.168.3.156
 WAITTIME=$1
